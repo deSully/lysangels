ed2e54f639c23fd34aa4c3ed88bf1e886aee0834
Williams De Souza <williams.stanley.desouza@gmail.com>
2026-01-17 23:06:00 +0100

    fix: wsgi uses prod settings by default

--- lysangels/settings/alpha.py
+++ lysangels/settings/alpha.py
@@ -106,6 +106,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', '/var/www/lysangels/media')
 
 
 # ==============================================================================
